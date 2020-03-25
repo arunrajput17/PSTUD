d95171fceb35102e1c775c7c25245d27ea84a19d
Arun Kumar <Arunk@ACROTECHNOLOGIES.COM>
2020-03-25 10:04:47 -0400

    If and for loops update

--- Conditional_Logic.py
+++ Conditional_Logic.py
@@ -33,3 +33,65 @@ if country.lower() == 'canada':
     print('Oh look a Canadian')
 else:
     print('You are not from Canada')
+
+
+#########################################################################
+
+num = input('Enter a number :')
+num=int(num)
+
+if num%2==0:
+    print('Number is even')
+else:
+    print('Number is odd')
+
+indian=['samosa','daal','naan']
+chinese=['egg role','pot sticker','fried rice']
+italian = ['pizza','pasta','risotto']
+
+dish=(input('Enter dish name: ')).lower()
+
+
+
+if dish in indian:
+    print('Indian cuisine')
+elif dish in chinese:
+    print('Chinese cuisine')
+elif dish in italian:
+    print('Italian Cuisine')
+else:
+    print('Based on little knowledge I have, I dont know which cuisine is ',dish)
+
+
+######################################
+usa=['atlanta','new york','chicago','baltimore']
+uk = ['london','bristol','cambridge']
+india=['mumbai','delhi','banglore']
+
+## Write a program that asks user to enter a city name and it should tell you which country it belongs to
+
+city =(input('Enter city name : ')).lower()
+
+if city in usa:
+    print('You are from USA')
+elif city in uk:
+    print('You are from UK')
+elif city in india:
+    print('You are from India')
+else:
+    print('Your country not known to me')
+
+##Write a program that asks user to enter two cities and tell you if they both are in same country or not
+
+city1 =(input('Enter your first city :')).lower()
+city2 = (input('Enter your second city :')).lower()
+
+if city1 in usa and city2 in usa:
+    print('Both cities belong to USA')
+elif city1 in uk and city2 in uk:
+    print('Both cities belong to UK')
+elif city1 in india and city2 in india:
+    print('Both cities belong to India')
+else:
+    print('Entered cities belong to different countries')
+
--- Loops.py
+++ Loops.py
@@ -19,3 +19,103 @@ while index < len(names):
     print(names[index])
     #Change the condition!!
     index= index + 1
+
+
+####################################################################################
+
+exp=[2340,2500,2100,3100,2980,]
+
+# total = exp[0]+exp[1]+exp[2]+exp[3]+exp[4]
+total =0
+for item in exp:
+    total = total + item
+print(total)
+
+for i in range(1,11):
+    print(i)
+
+## Monthwise expense and total expense
+total = 0
+for i in range (len(exp)):
+    print('Month:',(i+1),'Expense:',exp[i])
+    total = total + exp[i]
+
+print('Total expense is: ',total)
+
+## Find the key from differnt location in list
+
+key_location='chair'
+locations=['garage','living room','chair','closet']
+
+for i in locations:
+    if i==key_location:
+        print('Key is found in',i)
+        break
+    else:
+        print('Key is not found in', i)
+
+
+## Print square of numbers between 1 to 5 EXCEPT even numbers
+
+for i in range (1,6):
+    if i%2==0:
+        continue
+    print(i*i)
+
+
+i=1
+while i<=5:
+    print(i)
+    i=i+1
+
+
+## After flipping a coin 10 times got this result,
+## result = ['heads','tails','tails','heads','tails','heads','heads','tails','tails','tails']
+##Using for loop figure out count for 'heads'.
+
+result = ['heads','tails','tails','heads','tails','heads','heads','tails','tails','tails']
+count_heads=0
+for i in result:
+    if i=='tails':
+        continue
+    # print(i)
+    count_heads=count_heads+1
+print('Number of times heads came are: ',count_heads)
+
+## Another way
+c=0
+for i in result:
+    if i=='heads':
+        c=c+1
+    else:
+        continue
+print(c)
+
+
+## Your monthly expense list (from Jan to May) looks like this,
+## expense_list = [2340,2500,2100,3100,2980]
+## Write a program that asks you enter an expense amount and program should tell you the month in which that 
+##expense occured. If the expense is not found then convey that as well.
+
+expense_list = [2340,2500,2100,3100,2980]
+
+expense_amt = int(input('Enter the Expense amount: '))
+m=0
+for i in expense_list:
+    if i == expense_amt:
+        m=m+1
+        print('Month :',m, 'Expense :', expense_amt)
+        break
+    else:
+        m=m+1
+        print('not availble in Month: ',m)
+
+
+
+## Write a program that prints following shape,(Hint: Use for loop inside another for loop)
+##  * ** *** **** *****
+num=int(input('Enter number of rows: '))
+for i in range(1,num):
+    for j in range (1,i+1):
+        print('*',end=' ')
+    print()
