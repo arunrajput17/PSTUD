7b703504066ddec10d4116efd43512688baef974
Arun Kumar <Arunk@ACROTECHNOLOGIES.COM>
2020-03-27 09:25:03 -0400

    For loop changes

--- Functions.py
+++ Functions.py
@@ -70,4 +70,131 @@ print('Your initials are :' + get_initial(first_name) + get_initial(last_name))
 
 
 
+#################################################################
+
+## Function is a block of code that performs a specific task
+## Functions makes your code more modular and more readable.
+
+## Problem : You are given two lists of numbers and you need to find total of each of these list
+
+def calculate_total(exp):
+    total=0
+    for item in exp:
+        total =total+item
+    return total
+
+
+tom_exp_list=[2100,3400,3500]
+joe_exp_list=[200,500,700]
+
+toms_total= calculate_total(tom_exp_list)
+joes_total= calculate_total(joe_exp_list)
+
+print("Tom's total expenses:",toms_total)
+print("Joe's total expenses:", joes_total)
+
+def sum(a,b=0): ## b = 0 is assigned as default value
+    '''
+    This function takes two arguments which are integer numbers and
+    it will return sum of them as an output
+    '''
+    print('a:',a)
+    print('b:',b)
+    total=a+b
+    print('Total inside function:',total)
+    return total
+
+n=sum(5,6)
+print('Total outside function:',n)
+
+n=sum(19)
+print('Total outside function:',n)
+
+
+## Write a function called calculate_area that takes base and height as an input arguments and
+## returns as area of a triangle as an output . Here is the equation for an area of a triangle,
+## Triangle Area = 1/2*(base * height)
+
+def calculate_area(base, height):
+    Triangle_area = 1/2 * (base * height)
+    return Triangle_area
+
+Tri_area = calculate_area(3,3)
+print('Area of the triangle is:',Tri_area)
+
+
+## Modify above function to take third parameter called shape type. Shape type could be either
+## triangle or rectangle. Based on shape type it should calculate area. Equation for rectangle's area is,
+## Rectangle Area = length * width
+
+def calculate_area1(width, length, shape):
+    if shape == 'triangle':
+        Triangle_area = 1/2 * (width * length)
+        return Triangle_area
+    elif shape== 'rectangle':
+        Rectangle_area = (width * length)
+        return Rectangle_area
+    else:
+        print('Unknown shape type')
+
+Tri_area1 = calculate_area1(4,4,'triangle')
+print('Area of the triangle is:',Tri_area1)
+
+Rec_area1 = calculate_area1(4,4,'rectangle')
+print('Rectangle area is:',Rec_area1)
+
+Unk_area = calculate_area1(5,5,'xyz')
+
+
+## Write a function called print_pattern that takes integer number as argument and prints
+## following pattern if input number is 3,
+## * /n ** /n *** and If input is 4 then it should print, * /n ** /n *** /n ****
+
+def print_pattern(num):
+    for i in range(0,num):
+        for j in range (0,i+1):
+            print('*',end=' ')
+        print()
+
+
+print_pattern(3)
+print() 
+# print_pattern(4)
+
+## Reverse pattern
+
+def print_rev_pattern(num):
+    for i in range (num,0,-1):
+        for j in range(0, i):
+            print('*',end=' ')
+        print()
+
+
+print_rev_pattern (4)
+print()
+
+
+def print_revtriangle_pattern(num):
+    for i in range (num,0,-1):
+        for j in range(0, num-i):
+            print(end=' ')
+        for k in range (0,i):
+            print('*',end=' ')
+        print()
+
+
+print_revtriangle_pattern (4)
+print()
+
+def print_triangle_pattern(num):
+    for i in range (0,num):
+        # print('*',end=' ')
+        for _j in range (0,num-i):
+            print(end=' ')
+        for _k in range (0,i+1):
+            print('*',end=' ')
+        print()
+
+print_triangle_pattern(4)
+print()
 
--- Print.py
+++ Print.py
@@ -13,7 +13,7 @@ print()
 print('Adding numbers')
 x= 42 + 206
 print('Performing division')
-y= x/0
+y= x/1
 print('Math complete')
 
 
@@ -23,3 +23,54 @@ print('Math complete')
 
 x=5
 print('i am integer '+ str (x))
+
+
+###############################
+
+var1= 1
+var2 =2
+var3 = 3
+print(var1,var2,var3)
+
+## Using sep keyword in python print
+print(var1,var2,var3,sep='_')
+print(var1,var2,var3,sep='separator')
+
+####### Using end keyword in print
+
+initList = ['camel', 'case', 'stop']
+
+# print each words using loop
+print('Printing using default print function')
+for item in initList:
+    print(item)  # default print function. newline is appended after each item.
+
+print()  # another newline
+
+# print each words using modified print function
+print('Printing using modified print function')
+for item in initList:
+    print(item, end='-')
+
+##### file keyword in print
+
+# open a file in writable mood
+fi = open('output.txt', 'w')
+
+# initialize a list
+initList = ['Lion', 'case', 'stop']
+
+# print each words using loop
+print('Printing using default print function')
+for item in initList:
+    print(item, file=fi)  # use file keyword
+
+print(file=fi)
+
+# print each words using modified print function
+print('Printing using modified print function')
+for item in initList:
+    print(item, end='-', file=fi)  # use file keyword
+
+# close the file
+fi.close()
